08dd62fa6cf6bce832535ec408abfe524d2d5384
Isabela Grisales <126355153+IsabelaG22@users.noreply.github.com>
2025-06-20 08:03:09 -0500

    Delete Banner directory

--- Banner/script.js
+++
@@ -1,9 +0,0 @@
-const hamburgerMenu = document.querySelector('.hamburger-menu');
-const navLinks = document.querySelector('.nav-links');
-document.addEventListener('DOMContentLoaded', function () {
-
-    hamburgerMenu.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        hamburgerMenu.classList.toggle('active');
-    });
- })
